11b58dd086b78c5b6d7e9f5371b5e331ab8b4f30
Hamzah Fayad <hamzahfayad99@gmail.com>
2025-10-29 10:46:49 +0100

    feat: missing values indicator cols

--- 02_src/data_preprocessing.py
+++ 02_src/data_preprocessing.py
@@ -2,46 +2,39 @@
 
 import pandas as pd
 
-# Exclude Countries from DF with Missing Values Threshold >= 50%
+"""
+Exclude Countries from DF with Missing Values Threshold >= 50%
+"""
 THRESHOLD = 50
-def exclude_countries_high_missing_values(merged_df, total_values) -> pd.DataFrame:
+def exclude_countries_high_missing_values(merged_df) -> pd.DataFrame:
     
     all_missing_values = merged_df.isnull().groupby(level=0).sum()
+    # get sum of values per country for 9 main potential features: 
+    values_count_per_country = merged_df.groupby(level=0).size().iloc[0] * 9
+    
     all_missing_values["total_missing"] = all_missing_values.sum(axis=1)    #total missing values
-    all_missing_values["total_missing_%"] = round((all_missing_values["total_missing"] / total_values) * 100, 2)  #total missing values %
+    all_missing_values["total_missing_%"] = round((all_missing_values["total_missing"] / values_count_per_country) * 100, 2)  #total missing values %
 
     top_missing_countries = all_missing_values.sort_values(ascending=False, by="total_missing_%")
     exclude_countries = top_missing_countries[top_missing_countries["total_missing_%"] >= THRESHOLD]
 
     filtered_df_01 = merged_df[~merged_df.index.get_level_values(0).isin(exclude_countries.index.tolist())].copy()
 
-    #print("NEW FILTERED DF", filtered_df_01)
+    print("NEW FILTERED DF", filtered_df_01)
     return filtered_df_01
 
 
 """
-Handle missing values for column vaccination_coverage (6 NaNs for Nicaragua (NIC))
+Handle missing values #2: add missing values indicator (0 or 1)
+Make only for columns that have null values
+0 = not missing & 1 = missing
 """
-
-# def handle_missing_vaccination(filtered_df_01) -> pd.DataFrame:
-#     WORLDBANK_REG = "../00_data/1_interim/world-regions-worldbank.csv"
-#     COUNTRY_CODE = "NIC"
-#     COUNTRY_NAME = "Nicaragua"
-#     REGION = "Latin America and Caribbean (WB)"
-#     COL = "vaccination_coverage_who_unicef"
+def missing_values_indicator(filtered_df_01) -> pd.DataFrame:
     
-#     world_regions = pd.read_csv(WORLDBANK_REG)
-#     country_groups = world_regions.loc[:, ["Code", "World regions according to WB"]]
+    missing_indicators_df = filtered_df_01.copy()
     
-#     processed_df_01 = filtered_df_01.copy()
-#     processed_df_01 = processed_df_01.reset_index()
-#     processed_df_01 = processed_df_01.merge(country_groups, on=['Code'], how="left")
-#     processed_df_01 = processed_df_01.set_index(["Code", "Year"])
-    
-#     median_values = processed_df_01.groupby(["World regions according to WB"])[COL].median()
-#     median_val = median_values.loc[REGION]
-    
-#     filtered_df_01.loc[filtered_df_01["Entity"] == COUNTRY_NAME, COL] = median_val
-
-#     print("fill vaccination missing values: \n", filtered_df_01, filtered_df_01.isna().sum())
-#     return filtered_df_01
+    for column in missing_indicators_df.columns:
+        if missing_indicators_df[column].isna().sum() > 0:
+            missing_indicators_df[column+"_missing"] = missing_indicators_df[column].isna().astype(int)
+    print("DF with missing indicators", missing_indicators_df)
+    return missing_indicators_df
