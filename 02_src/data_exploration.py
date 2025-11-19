fcf7a7ce6d082090b5c1b9b083c795e47ec88352
Hamzah Fayad <hamzahfayad99@gmail.com>
2025-11-19 18:00:29 +0100

    feat: test descriptive imputation + multivariate imputation//start strategic comparison

--- 02_src/data_exploration.py
+++ 02_src/data_exploration.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
 # Reusable Functions - Data Exploration
 
 """
@@ -20,4 +23,10 @@ def scatterplots(df, features, cols_num):
     for id, a in enumerate(axs):
         sns.scatterplot(data=df, x="child_mortality_igme", y=features[id], hue=features[id], 
                         size=features[id], sizes=(25, 100), ax=axs[id])
+    plt.show()
+    
+    
+def histograms(df):
+    df.hist(figsize=(14,8))
+    plt.tight_layout()
     plt.show()
